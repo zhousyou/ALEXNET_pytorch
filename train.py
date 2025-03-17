b635ee99d1d27b6591682d43bfc80fe7931f4d43
zzy <1597463346@qq.com>
2025-03-17 16:11:15 +0800

    修改train.py代码，处理训练逻辑

--- train.py
+++ train.py
@@ -64,6 +64,29 @@ model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(params=model.parameters(), lr=lr)
 
+def test_train():
+    model.train()
+    total_loss = 0
+    for img, label in train_loader:
+        print(img.shape, label.shape)  #img:128,3,224,224 label:128
+        optimizer.zero_grad()
+        img = img.to(device)
+        label = label.to(device)  
+        #前向传播
+        outputs = model(img)  #128*2
+        max_vaules, max_indexed = torch.max(outputs, 1)
+        print(max_indexed.shape)
+        # print(outputs.shape)
+        #计算损失
+        loss = criterion(outputs, label)
+        print(loss)
+    # img, label = train_dataset[0]
+    # print(img.shape)
+    # print(label)
+
+        
+
+
 #定义训练的函数
 def train():
     best_acc = 0.0
@@ -88,14 +111,14 @@ def train():
 
         #验证
         epoch_loss = running_loss/len(train_loader.dataset)
-        val_loss, val_acc = validation()
+        # val_loss, val_acc = validation()
         
-        print(f'Epoch [{e+1}/{epoch}]')
-        print(f'Train Loss: {epoch_loss:.4f} | Val Loss: {val_loss:.4f} | Val Acc: {val_acc:.2f}%')
+        print(f'Epoch [{e+1}/{epoch}], LOSS: {epoch_loss: .4f}')
+        # print(f'Train Loss: {epoch_loss:.4f} | Val Loss: {val_loss:.4f} | Val Acc: {val_acc:.2f}%')
         # 保存最佳模型
-        if val_acc > best_acc:
-            best_acc = val_acc
-            torch.save(model.state_dict(), 'best_model.pth')
+        # if val_acc > best_acc:
+        #     best_acc = val_acc
+        #     torch.save(model.state_dict(), 'best_model.pth')
 
 
 #验证的函数
@@ -112,10 +135,9 @@ def validation():
 
             outputs = model(image)
             loss = criterion(outputs, label)
-            predicted = torch.argmax(outputs).item()
-            total += label.size(0)
-            if predicted==label:
-                correct += 1
+            _, predicted = torch.argmax(outputs,1).item()  #torch.max()返回两个值：max_values, max_indexes;outputs.shape:(128,2);predicted.shpae:128
+            total += label.size(0)   #label.shape:128
+            correct += (label==predicted).sum().item()
             running_loss += loss.item()*image.size(0)
         val_loss = running_loss/len(val_loader.dataset)
         val_acc = correct*100/total
@@ -123,5 +145,6 @@ def validation():
 
 
 if __name__=="__main__":
+    # test_train()
+    # print(train_loader[0])
     train()
-
